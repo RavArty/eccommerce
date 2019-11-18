be9da938eac2580620fddd6f01fe44ee093fdfee
Ravshan <ravkart@gmail.com>
2019-11-18 11:52:11 -0800

    added router-dom, splited homepage to directory and menu-item

---
+++ src/components/menu-item/menu-item.component.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+interface IMenuItemProps {
+  title: string;
+  imageUrl: string;
+  size: string | undefined;
+}
+const MenuItem = ({ title, imageUrl, size }: IMenuItemProps): JSX.Element => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
