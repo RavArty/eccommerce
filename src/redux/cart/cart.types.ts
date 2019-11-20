1268b19d7ccdc20d1b1a5ff0fb4c65944fcae6b3
Ravshan <ravkart@gmail.com>
2019-11-20 15:50:50 -0800

    added removeItem and addItem to checkout page

--- src/redux/cart/cart.types.ts
+++ src/redux/cart/cart.types.ts
@@ -1,8 +1,19 @@
-import { ToggleCartAction, AddItemAction } from './cart.actions';
+import {
+  ToggleCartAction,
+  AddItemAction,
+  ClearFromCartAction,
+  RemoveItemFromCartAction
+} from './cart.actions';
 
 export enum CartActionTypes {
   TOGGLE_CART_HIDDEN = 'TOGGLE_CART_HIDDEN',
-  ADD_ITEM = 'ADD_ITEM'
+  ADD_ITEM = 'ADD_ITEM',
+  CLEAR_ITEM_FROM_CART = 'CLEAR_ITEM_FROM_CART',
+  REMOVE_ITEM = 'REMOVE_ITEM'
 }
 
-export type CartActions = ToggleCartAction | AddItemAction;
+export type CartActions =
+  | ToggleCartAction
+  | AddItemAction
+  | ClearFromCartAction
+  | RemoveItemFromCartAction;
--- src/redux/cart/cart.utils.ts
+++ src/redux/cart/cart.utils.ts
@@ -18,3 +18,22 @@ export const addItemToCart = (
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (
+  cartItems: ITypeData[],
+  cartItemToRemove: ITypeData
+) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem!.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity! - 1 }
+      : cartItem
+  );
+};
