8db3e49aa72de0ba14c3503127a9b10c9a1c3096
Ravshan <ravkart@gmail.com>
2019-11-24 15:37:42 -0800

    add withSpinner to shop component

--- src/pages/shop/shop.component.tsx
+++ src/pages/shop/shop.component.tsx
@@ -11,13 +11,21 @@ import {
 
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 interface ISHopPageProps extends RouteComponentProps {
   updateCollections: any;
 }
 class ShopPage extends React.Component<ISHopPageProps, {}> {
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -37,12 +45,21 @@ class ShopPage extends React.Component<ISHopPageProps, {}> {
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
