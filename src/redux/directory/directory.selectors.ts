19aec49829257e81138241ced6451092d8cd6a5f
Ravshan <ravkart@gmail.com>
2019-11-26 22:07:14 -0800

    added package and server files

--- src/redux/directory/directory.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import { AppState } from '../root-reducer';
-
-const selectDirectory = (state: AppState) => state.directory;
-
-export const selectDirectorySections = createSelector(
-  [selectDirectory],
-  directory => directory.sections
-);
--- src/redux/user/user.types.ts
+++
@@ -1,7 +0,0 @@
-import { SetCurUserAction } from './user.actions';
-
-export enum UserActionTypes {
-  SET_CURRENT_USER = 'SET_CURRENT_USER'
-}
-
-export type UserAction = SetCurUserAction;
