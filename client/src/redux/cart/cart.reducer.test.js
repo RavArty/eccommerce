0490803c5675effcdeac6e6c03278eb808837bd7
Ravshan <ravkart@gmail.com>
2019-12-04 14:51:07 -0800

    added test for card.reducer

--- client/src/redux/cart/cart.reducer.test.js
+++
@@ -1,4 +0,0 @@
-import { cartReducer } from './cart.reducer';
-
-test('returns default initial state of `false` when no action is passed', () => {});
-test('returns state of true upon receiving an action of type `CORRECT_GUESS`', () => {});
