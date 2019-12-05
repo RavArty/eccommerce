34a1ce71f444246aa63b99c3c9f532681d8725f9
Ravshan <ravkart@gmail.com>
2019-12-04 17:02:18 -0800

    added user redux tests

---
+++ client/src/redux/user/userTests/user.reducer.test.js
@@ -0,0 +1,23 @@
+import { UserActionTypes } from '../user.types';
+import userReducer from '../user.reducer';
+
+const initialState = {
+  currentUser: null
+};
+
+describe('userReducer', () => {
+  it('should return initial state', () => {
+    expect(userReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should set currentUser to payload on setCurrentUser action', () => {
+    const mockUser = { id: 1, displayName: 'Rav' };
+
+    expect(
+      userReducer(initialState, {
+        type: UserActionTypes.SET_CURRENT_USER,
+        payload: mockUser
+      }).currentUser
+    ).toEqual(mockUser);
+  });
+});
